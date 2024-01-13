6452e9223fcf2311b09bfac2b4c09647e4a25174
Misha <example@gmail.com>
2024-01-13 20:29:52 +0200

    small refactoring

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,20 +5,11 @@ from utils.bubble_page import BubblePage
 from utils.hamburger_menu import HamburgerMenu
 
 
-# @pytest.mark.usefixtures("screen_recorder")
-# def test_example(driver):
-#     bubble_page = BubblePage(driver)
-#     bubble_page.click_hamburger()
-#     assert driver.find_element(AppiumBy.ID, 'com.ideomobile.maccabi:id/search_icon').is_displayed()
-
-
 @pytest.mark.usefixtures("screen_recorder")
 def test_example(driver):
     bubble_page = BubblePage(driver)
     hamburger_menu = HamburgerMenu(driver)
     if bubble_page.popup_we_together():
         bubble_page.click_on_scape()
-    else:
-        pass
     bubble_page.click_hamburger()
     assert hamburger_menu.quick_look().text == 'מבט מהיר'
